655a2fc6353d4dc4afeb0777474bc9801c1e06c5
oxana-ermolenko <oxana.ermolenko.dev@gmail.com>
2020-06-12 04:00:53 -0700

    made authentication routing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -13,8 +16,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 5,
-    maxlength: 10
+    minlength: 5
   },
   lastname: {
     type: String,
@@ -32,6 +34,54 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if(user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+});
+
+userSchema.methods.comparePassword = function(plainRassword, cb) {
+  bcrypt.compare(plainRassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), 'secret')
+
+  user.token = token;
+  user.save(function (err, user) {
+    if(err) return cb(err)
+    cb(null, user);
+  })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+
+  jwt.verify(token, 'secret', function(err, decode) {
+    user.findOne({
+      "_id": decode,
+      "token": token},
+      function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+      })
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
