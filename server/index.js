ca4956a77836edee4a12bad68c1a6e34a418a752
oxana-ermolenko <oxana.ermolenko.dev@gmail.com>
2020-06-12 05:29:27 -0700

    heroku again

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json({"hello": "I am happy to deploy our application"})
+})
+
 app.get('/api/user/auth', auth,  (req, res) => {
   res.status(200).json({
     _id: req._id,
